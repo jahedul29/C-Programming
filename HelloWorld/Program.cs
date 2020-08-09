8fa276816bc121b24ccb54f23cede444a819e284
jahedul29 <jahedulh1@gmail.com>
2020-08-09 14:39:36 +0600

    variables

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    using System.Dynamic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lang = Console.ReadLine();
+            int num = Convert.ToInt32(Console.ReadLine());
+            double point = Convert.ToDouble(Console.ReadLine());
+            bool flag = Convert.ToBoolean(Console.ReadLine());
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(lang);
+            Console.WriteLine(num);
+            Console.WriteLine(point);
+            Console.WriteLine(flag);
+            Console.WriteLine(ch);
+            Console.ReadLine();
+        }
+    }
+}
